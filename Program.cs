d4c60b5965677e16493684065d2d4aa093e50dd1
Tasha Lane <designtmg77@gmail.com>
2020-08-27 22:28:59 -0500

    quotient and remainder

--- Program.cs
+++ Program.cs
@@ -6,28 +6,47 @@ namespace NumericTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lets Add!");
+            // Console.WriteLine("Lets Add!");
 
-            int a = 18;
-            int b = 6;
-            int c = a + b;
-            Console.WriteLine(c);
+            // int a = 18;
+            // int b = 6;
+            // int c = a + b;
+            // Console.WriteLine(c);
 
-            Console.WriteLine("Lets Subtract!");
+            // Console.WriteLine("Lets Subtract!");
 
-            int d = a - b;
-            Console.WriteLine(d);
+            // int d = a - b;
+            // Console.WriteLine(d);
 
-            Console.WriteLine("Lets Multiply!");
+            // Console.WriteLine("Lets Multiply!");
 
-            int e = a * b;
-            Console.WriteLine(e);
+            // int e = a * b;
+            // Console.WriteLine(e);
 
-            Console.WriteLine("Lets Divide!");
+            // Console.WriteLine("Lets Divide!");
 
-            int f = a / b;
-            Console.WriteLine(f);
+            // int f = a / b;
+            // Console.WriteLine(f);
 
+            // Console.WriteLine("Lets Do Order of Operations!");
+
+            // int w = 5;
+            // int x = 4;
+            // int y = 2;
+            // int z = w + x * y;
+            // Console.WriteLine(d);
+
+            Console.WriteLine("Lets Do a Combination!");
+
+            // int mdas = (w + x) - 6 * y + (12 * 4) / 3 + 12;
+
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
         }
     }
 }
