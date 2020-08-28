ef3228b463a1a4c8d890ea6920d22b36d053fda0
Tasha Lane <designtmg77@gmail.com>
2020-08-27 22:16:10 -0500

    mdas

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets Add!");
+
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            Console.WriteLine("Lets Subtract!");
+
+            int d = a - b;
+            Console.WriteLine(d);
+
+            Console.WriteLine("Lets Multiply!");
+
+            int e = a * b;
+            Console.WriteLine(e);
+
+            Console.WriteLine("Lets Divide!");
+
+            int f = a / b;
+            Console.WriteLine(f);
+
+        }
+    }
+}
